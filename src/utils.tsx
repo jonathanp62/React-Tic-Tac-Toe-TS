bad350defc5ce45cc19817480a826a0b970671d5
Jonathan Parker <jonathanp62@gmail.com>
2025-10-15 12:25:51 -0400

    Initial 0.1.0 commit

---
+++ src/utils.tsx
@@ -0,0 +1,83 @@
+/*
+ * (#)utils.tsx 0.1.0   10/15/2025
+ *
+ * @author  Jonathan Parker
+ * @version 0.1.0
+ * @since   0.1.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import Square from "./Square.tsx";
+
+/**
+ * Calculates the winner of the game.
+ *
+ * @param   {Array<Square>} squares
+ * @returns                 {string|null}
+ */
+export function calculateWinner(squares: Array<typeof Square>): typeof Square | null {
+    // Winning boards
+
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
+/**
+ * Return true if the game is over, i.e. a draw.
+ * The condition is that all squares are occupied.
+ *
+ * @param   {Array<Square>} squares
+ * @returns                 {boolean}
+ */
+export function isGameOver(squares: Array<typeof Square>): boolean {
+    let isGameOver = true;
+
+    for (const square of squares) {
+        if (!square) {
+            isGameOver = false;
+
+            break;
+        }
+    }
+
+    return isGameOver;
+}
